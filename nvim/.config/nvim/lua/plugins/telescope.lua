32359c3b344228517b042e555bd9faaeceeb15b6
Mateusz Kłosiński <mkklosin@gmail.com>
2022-02-15 20:30:44 +0100

    Removed redudant configuration

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,32 +2,6 @@ local telescope = require 'telescope'
 local actions = require 'telescope.actions'
 local keymap = require 'lib.utils'.keymap
 
--- local telescope = require 'telescope'
--- local keymap = require 'lib.utils'.keymap
-
--- telescope.setup {
---    defaults = {
---        color_devicons = true,
---       layout_config = {
---            prompt_position = 'top',
---        },
---        file_ignore_patterns = { '.git/', 'node_modules/' },
---    },
---    pickers = {
---        sort_lastused = true,
---    },
---    extensions = {
---        fzf = {
---            fuzzy = true,
---            override_generic_sorter = true,
---            override_file_sorter = true,
---            case_mode = 'smart_case'
---        }
---    }
--- }
-
--- require('telescope').load_extension 'fzf'
-
 keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<CR>]])
 keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]])
 keymap('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<CR>]])
