55b7cdfd36c897e4aa8bd14b36d156af435476b1
Mateusz Kłosiński <mkklosin@gmail.com>
2022-02-18 07:53:45 +0100

    Updated `nvim-lsp-installer` config

---
+++ nvim/.config/nvim/lua/plugins/lsp/lsp-installer.lua
@@ -0,0 +1,48 @@
+local status_ok, lsp_installer = pcall(require, 'nvim-lsp-installer')
+
+if not status_ok then
+  return
+end
+
+lsp_installer.settings({
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
+    }
+  }
+})
+
+local servers = {
+  'sumneko_lua',
+  'tsserver',
+  'tailwindcss',
+  'html',
+  'eslint',
+  'html',
+  'emmet_ls',
+  'bashls',
+}
+
+for _, name in pairs(servers) do
+  local server_is_found, server = lsp_installer.get_server(name)
+  if server_is_found and not server:is_installed() then
+    print("Installing " .. name)
+    server:install()
+  end
+end
+
+lsp_installer.on_server_ready(function(server)
+  local opts = {
+    on_attach = require('plugins/lsp/handlers').on_attach,
+    capabilities = require('plugins/lsp/handlers').capabilities
+  }
+
+  if server.name == 'sumneko_lua' then
+    local sumneko_opts = require('plugins.lsp.sumneko_lua')
+    opts = vim.tbl_deep_extend('force', sumneko_opts, opts)
+  end
+
+  server:setup(opts)
+end)
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -4,6 +4,42 @@ packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'kyazdani42/nvim-web-devicons'
   use 'nvim-lua/plenary.nvim'
+  use 'neovim/nvim-lspconfig'
+-- use 'williamboman/nvim-lsp-installer'
+
+--  use {
+--    'neovim/nvim-lspconfig',
+--    config = function()
+--      require('plugins.lsp-config')
+--    end
+--  }
+
+  use {
+    'williamboman/nvim-lsp-installer',
+    requires = {
+      'neovim/nvim-lspconfig'
+    },
+    config = function()
+      require('plugins.lsp')
+    end
+  }
+
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'saadparwaiz1/cmp_luasnip',
+      'L3MON4D3/LuaSnip',
+      'jessarcher/cmp-path',
+      'hrsh7th/cmp-nvim-lua',
+      'onsails/lspkind-nvim',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+    },
+    config = function()
+      require('plugins.cmp')
+    end
+  }
 
   use {
     'kyazdani42/nvim-tree.lua',
@@ -18,7 +54,7 @@ packer.startup(function(use)
     config = function()
       require('plugins.dracula')
     end
-  } 
+  }
 
   use {
     'nvim-telescope/telescope.nvim',
@@ -37,6 +73,13 @@ packer.startup(function(use)
       config = function()
           require('plugins.nvim-treesitter')
       end
+  }
+
+  use {
+    'glepnir/dashboard-nvim',
+    config = function()
+      require('plugins.dashboard')
+    end
   }
 
   if PACKER_BOOTSTRAP then
